c7cc3a249e3e59a2b72c4fd5054091bcf6844d59
Zchwazer <yamiyomuyumi@gmail.com>
2019-08-20 13:45:19 +0700

    Change path
    Change path from employees to members

--- src/main/java/com/practice/demo/dao/EmployeeRepository.java
+++ src/main/java/com/practice/demo/dao/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.practice.demo.dao;
 
 import com.practice.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
 }
